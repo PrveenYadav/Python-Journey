31afd9671a4f40025766826e9d703b83abea264d
PrveenYadav <bikyyadav13@gmail.com>
2025-07-10 20:03:53 +0530

    behind the scene of loops and iterable tools

---
+++ Python Journey 2.0/03_conditions and loops/code2.py
@@ -0,0 +1,22 @@
+# Iterable tools
+# code in terminal
+file = open('code2.py')
+file.readline() 
+file.__next__()
+
+
+username = "Vicky Choudhary"
+print(username)
+
+test = ""
+test_one = "Aman"
+
+# not keyword
+if not test:
+    print("Vicky")
+
+if not test_one:
+    print("Choudhary")
+
+# is keyword
+print(test is test_one)
