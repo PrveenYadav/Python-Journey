fd7c2c4a8a316b6de8ed6a5418e80a7d7b0a5e0c
PrveenYadav <bikyyadav13@gmail.com>
2025-11-02 18:55:26 +0530

    solve container with most water problem

---
+++ DSA with Python/Neetcode-150/Two_Pointers/day-4.py
@@ -0,0 +1,41 @@
+# 
+
+class Solution:
+    def __init__(self):
+        pass
+    
+    # Container with most water
+    def mostWater(self, nums): # Brute force : O(n^2), O(1)
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                ht = min(nums[i], nums[j])
+                area = j - i
+                currWater = ht * area
+                ans = max(ans, currWater)
+        return ans
+    
+    # Two Pointer
+    def mostWater1(self, height): # O(n), O(1)
+        ans = 0
+        i, j = 0, len(height)-1
+
+        while i < j:
+            ht = min(height[i], height[j])
+            area = j - i
+            currWater = ht * area
+            ans = max(ans, currWater)
+
+            if height[i] <= height[j]:
+                i += 1
+            else:
+                j -= 1
+        
+        return ans
+
+if __name__ == "__main__":
+    myObj = Solution()
+    height = [1,8,6,2,5,4,8,3,7]
+
+    print(myObj.mostWater(height))
+    print(myObj.mostWater1(height))
